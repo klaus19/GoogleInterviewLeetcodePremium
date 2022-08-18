eda644ff1beb38cf09a9b0331387f6f5820ea03f
tejaskhartude <tejaskhartude@gmail.com>
2022-08-18 20:14:46 +0530

    Add SymmetryTree problem

---
+++ src/ArrayandString/SymmetryTree.java
@@ -0,0 +1,22 @@
+package ArrayandString;
+
+public class SymmetryTree {
+
+    public boolean isSymmetric(TreeNode root){
+        return isMirror(root,root);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+
+        if (t1==null && t2==null){
+            return true;
+        }
+
+        if (t1==null || t2==null){
+            return false;
+        }
+
+        return (t1.val== t2.val) && isMirror(t1.left,t2.right) && isMirror(t1.right,t2.left);
+
+    }
+}
---
+++ src/ArrayandString/TreeNode.java
@@ -0,0 +1,19 @@
+package ArrayandString;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){
+
+    }
+
+    TreeNode(int val){
+        this.val=val;
+    }
+    TreeNode(int val,TreeNode left,TreeNode right){
+        this.val=val;
+        this.right=right;
+
+    }
+}
